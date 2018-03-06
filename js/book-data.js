a961d8fbb94f2358444adab64b55b1d1f67202d2
Jesie Oldenburg <kitglo46@gmail.com>
2018-03-06 14:26:37 -0600

    index.html changes : added a button for the form; book data: added variables to title search function and button event listener; main.js: added book search var and requires module data;

--- js/book-data.js
+++ js/book-data.js
@@ -1,16 +1,31 @@
 "use strict";
 
+console.log("Book Data");
+
 //Create a function that stores the user's input in a var, then add that value to the end of the api url.
 
 // userBook input will grab their input using document.getElementById().value.... Then, encodeURI the user input, and pass it to the userInputTo 
 
-var userBookTitleSearch = $("#book-title").val();
-var userAuthorSearch = $("#author-name").val();
-var userPublishSearch = $("#publish-year").val();
+
+
+let bookSubmitButton = $("#form-submit");
+console.log("What is the button?", bookSubmitButton);
+
+bookSubmitButton.click(userInputToURL(event));
 
 
 function userInputToURL (string) {
 	
+	let bookTitleSearch = $("#book-title").val();
+	console.log("book title", bookTitleSearch);
+
+	let authorSearch = $("#author-name").val();
+	console.log("authorSearch", authorSearch);
+
+	let yearPublishedSearch = $("#publish-year").val();
+	console.log("yearPublishedSearch", yearPublishedSearch);
+
+
 }
 
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 "use strict";
 let news = require("./news");
+let bookSearch = require("./book-data");
 
 
 console.log("hello world");
