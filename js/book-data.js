593b4979ca96ef9b3767b677850d55b2636c4db0
Ben Atkins <benjatkins13@gmail.com>
2018-03-06 12:48:15 -0600

    final fix

--- js/book-data.js
+++ js/book-data.js
@@ -1,5 +1,5 @@
 "use strict";
-let $ = require('jquery');
+
 //Create a function that stores the user's input in a var, then add that value to the end of the api url.
 
 // userBook input will grab their input using document.getElementById().value.... Then, encodeURI the user input, and pass it to the userInputTo 
