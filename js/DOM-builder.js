e462444d98ba1d5bbc644b6d0a3b77b20f9c5daa
Arthur Smith <arthursmith@ms-MacBook-Pro.local>
2018-03-07 15:21:27 -0600

    doing some stuff with user logins and logouts and buttons

---
+++ js/DOM-builder.js
@@ -0,0 +1,10 @@
+'use strict';
+//IF THE USER IS LOGGED IN, HIDE THE LOG IN BUTTON
+//ELSE USER IS LOGGED OUT, HIDE LOG OUT BUTTON
+if (currentuser === loggedin) {
+    $('#login').attr('hidden', true);
+    $('#logout').removeAttr('hidden', true);
+}
+else (currentuser === loggedOut) {
+    $('#logout').attr('hidden', true);
+}
