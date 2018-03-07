aa8af7d27c5e649fee0b144bdca4579ff2fe915c
Jesie Oldenburg <kitglo46@gmail.com>
2018-03-07 10:27:43 -0600

    book_data_fetch.js : renamed file; got focus out event listeners working. main.js: amended doc path inside of bookSearch variable.

---
+++ js/book_data_fetch.js
@@ -0,0 +1,62 @@
+"use strict";
+
+console.log("Books Data On Station");
+
+//Create a function that stores the user's input in a var, then add that value to the end of the api url.
+
+// userBook input will grab their input using document.getElementById().value.... Then, encodeURI the user input, and pass it to the userInputTo 
+
+
+
+let bookSubmitButton = $("#form-submit");
+
+
+var titleInput = $("#book-title");
+var string = "";
+
+
+
+var titleField = $("#book-title");
+var authorField = $("#author-name");
+var yearPubField = $("#publish-year");
+let bookForm = $("book-form");
+
+
+titleField.focusout(function (event) {
+	console.log("focus out fired", titleInput.val());
+	// return titleField.val();
+});
+
+authorField.focusout(function (event) {
+	console.log("authorField focus out", authorField.val()); 
+});	
+
+yearPubField.focusout(function (event) {
+	console.log("year pub field focus out", yearPubField.val());
+});
+
+function userInputToURL (event, string) {
+	// console.log(titleField);
+	
+}
+
+bookSubmitButton.click(function (event) {
+	console.log("Click me again");
+});
+
+
+
+
+
+
+
+function grabBookData(userBookInput) {
+   
+	return $.ajax({
+		url: `https://openlibrary.org/search.json?q=${userBookInput}`,
+
+	}).done(() => {
+
+		return ;
+   });
+}
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 let news = require("./news");
-let bookSearch = require("./book-data");
+let bookSearch = require("./book_data_fetch.js");
 
 
 console.log("hello world");
