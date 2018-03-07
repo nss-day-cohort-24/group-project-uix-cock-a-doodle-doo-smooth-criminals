4e28cca1b6e722f910066c675d43cd28da30d88e
Ben Atkins <benjatkins13@gmail.com>
2018-03-07 11:12:11 -0600

    ba-fb

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 "use strict";
 let bootstrap = require("../lib/node_modules/bootstrap");
 let news = require("./news");
+let user = require("./user");
 
 let bookSearch = require("./book_data_fetch.js");
 
-let loginBtn = document.getElementById("login");
 
-
+$("#login").click(function() {
+    console.log("clicked auth");
+    user.logInGoogle()
+    .then((result) => {
+      console.log("result from login", result.user.uid);
+      // user = result.user.uid;
+      user.setUser(result.user.uid);
+    //   $("#auth-btn").addClass("is-hidden");
+    //   $("#logout").removeClass("is-hidden");
+    });
+  });
 
 
 console.log("hello world");
