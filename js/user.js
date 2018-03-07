24f7cebeba2193f9525e865cbea832f069affe91
Ben Atkins <benjatkins13@gmail.com>
2018-03-07 10:10:17 -0600

    trying fb

---
+++ js/user.js
@@ -0,0 +1,36 @@
+"use strict";
+//install firebase into lib folder npm install firebase --save
+let firebase = require("./fb-config"),
+	 provider = new firebase.auth.GoogleAuthProvider(),
+	 currentUser = null;
+
+//listen for changed state
+firebase.auth().onAuthStateChanged((user) => {
+	console.log("onAuthStateChanged", user);
+	if (user){
+		currentUser = user.uid;
+		console.log("current user Logged in?", currentUser);
+	}else {
+		currentUser = null;
+		console.log("current user NOT logged in:", currentUser);
+	}
+});
+
+function logInGoogle() {
+	//all firebase functions return a promise!! Add a then when called
+
+	return firebase.auth().signInWithPopup(provider);
+}
+
+function logOut(){
+	return firebase.auth().signOut();
+}
+function getUser(){
+	return currentUser;
+}
+
+function setUser(val){
+	currentUser = val;
+}
+
+module.exports = {logInGoogle, logOut, getUser, setUser};
