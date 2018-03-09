91f0a4b3ca0b1486094a5e9d0c73dc9e0c0b80a3
Jesie Oldenburg <kitglo46@gmail.com>
2018-03-09 00:11:54 -0600

    got functionality of forms back, and loaded api query's correctly;  Next step print card in the dom...

--- js/book_data_fetch.js
+++ js/book_data_fetch.js
@@ -5,63 +5,92 @@ console.log("Books Data On Station");
 
 // userBook input will grab their input using document.getElementById().value.... Then, encodeURI the user input, and pass it to the userInputTo 
 
-var apiURL = ``;
-
-
+var URLQueryString = ``;
+var encodedURLString;
+var bookData;
 let bookSubmitButton = $("#form-submit"); // The button to submit the form...
 let titleField = $("#book-title"); // The form field for the book title...
 let authorField = $("#author-name"); // The form field for the author name field...
 let yearPubField = $("#publish-year"); //The form field for the year published field...
 
 
+function userInputToURL(event, element) {
+    
+    // let titleQuery = titleField.val();
+    // console.log("BOOM", titleQuery);
+   
+    // let authorQuery = authorField.val();
+    // console.log("Goes", authorQuery);
 
-function userInputToURL(element, apiURL) {
-    $.holdReady(false);
-    console.log(titleField.val());
-    let titleQuery = titleField.val();
-    console.log("here is your title query brah", titleQuery);
+    // let yearQuery = yearPubField.val();
+    // console.log("The dynomiteeee!", yearQuery);
 
-    apiURL = `http://openlibrary.org/search.json?q=${titleQuery}`;
-
-    let encodedURL = apiURL.encodeURI(apiURL);
-    console.log("what is encoded url", encodedURL);
-    console.log("here is your encoded url brah", apiURL);
+    // URLQueryString = `http://openlibrary.org/search.json?q=${titleQuery}+${authorQuery}+${yearQuery}`;
+    // let encodedURLString = encodeURI(URLQueryString);
+    // console.log("what is encoded url", encodedURLString);
 }
 
 
-titleField.focusout(function(e) {
-    console.log(titleField.val());
+titleField.focusout(function (event) {
 
     let titleQuery = titleField.val();
-    console.log("here is your title query brah", titleQuery);
+    
+    URLQueryString = `${titleQuery}`;
+    
 
-    apiURL = `http://openlibrary.org/search.json?q=${titleQuery}`;
-
-    let encodedURL = apiURL.encodeURI(apiURL);
-    console.log("what is encoded url", encodedURL);
-    console.log("here is your encoded url brah", apiURL);
 });
 
-authorField.focusout(userInputToURL(event, apiURL));
+authorField.focusout(function(event) {
+    if (authorField.val === null) { 
+        alert("Please Enter A Character");
+    } else {
+            
+    let authorQuery = authorField.val();
+    
+    
+    URLQueryString += `${authorQuery}`;
+    
+}
+});
 
-yearPubField.focusout(userInputToURL(event, apiURL));
+yearPubField.focusout(function(event) {
+    
+    let yearQuery = yearPubField.val();
+    
+
+    URLQueryString += `${yearQuery}`;
+    
+
+    encodedURLString = encodeURI(URLQueryString).toLowerCase();
+    
+    // return encodedURLString;
+    grabBookData(encodedURLString);
+});
+
+function displayBookResults (bookData) {
+    console.log("book results is here, check if this puppy works");
+}
 
 
+
+function grabBookData(string) {
+
+    return $.ajax({
+        url: `https://openlibrary.org/search.json?q=${encodedURLString}`,
+
+    }).done((bookData) => {
+        console.log("Should be info about a book", bookData);
+        displayBookResults(bookData);
+    });
+}
+console.log("WHAT IS MAH BOOK DATA!!", bookData);
 
 bookSubmitButton.click(function(event) {
     console.log("Click me again");
+    alert("Processing your request Mah Lord!");
 });
 
 
-function grabBookData(userBookInput) {
 
-    return $.ajax({
-        url: `https://openlibrary.org/search.json?q=${userBookInput}`,
-
-    }).done(() => {
-
-        return;
-    });
-}
 
 module.exports = { userInputToURL };
